b1060bd8583f2616f47f77f093a38e32bb7ac06e
Saurabh Kumar <sauravgautam884@gmail.com>
2021-09-08 22:44:37 +0530

    console.log debugs

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 import "firebase/storage";
-let object = require("./secrets");
+let object = require("./secrets")
 firebase.initializeApp(object);
 let auth = firebase.auth();
 export default auth
